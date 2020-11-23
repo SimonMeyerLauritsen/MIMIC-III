191bd36cd344cc96d9a5f941783f2fb49e1bf7d4
Simon Meyer Lauritsen <51704568+SimonMeyerLauritsen@users.noreply.github.com>
2020-11-23 12:08:25 +0100

    Create InsertSuspinfect_poe_iv.sql

---
+++ InsertSuspinfect_poe_iv.sql
@@ -0,0 +1,155 @@
+CREATE PROCEDURE [dbo].[InsertSuspinfect_poe_iv]
+AS
+/*
+- ORIGINAL VERSION: https://github.com/alistairewj/sepsis3-mimic/blob/master/query/tbls/abx-micro-prescription.sql
+- DOWNLOADED FROM (MODIFIED VERSION): https://github.com/BorgwardtLab/mgp-tcn/tree/master/src/query
+*/
+
+-- Checked for DATEADD
+
+-- only works for metavision as carevue does not accurately document antibiotics
+DROP TABLE IF EXISTS dbo.suspinfect_poe_iv;
+with mv as
+(
+  select hadm_id
+  , mv.drug as antibiotic_name
+  , startdate as antibiotic_time
+  , enddate as antibiotic_endtime
+  from prescriptions mv
+  inner join abx_poe_list ab
+      on mv.drug = ab.drug
+)
+-- get cultures for each icustay
+-- note this duplicates data across ICU stays for the same hospitalization
+-- we later filter down to first ICU stay so not concerned about it
+, ab_tbl as
+(
+  select
+        ie.subject_id, ie.hadm_id, ie.icustay_id
+      , ie.intime, ie.outtime
+      , mv.antibiotic_name
+      , mv.antibiotic_time
+      , mv.antibiotic_endtime
+      -- , ROW_NUMBER() over
+      -- (
+      --   partition by ie.icustay_id
+      --   order by mv.antibiotic_time, mv.antibiotic_endtime
+      -- ) as rn
+  from icustays ie
+  left join mv
+      on ie.hadm_id = mv.hadm_id
+)
+, me as
+(
+  select hadm_id
+    , chartdate, charttime
+    , spec_type_desc
+    , max(case when org_name is not null and org_name != '' then 1 else 0 end) as PositiveCulture
+  from microbiologyevents
+  group by hadm_id, chartdate, charttime, spec_type_desc
+)
+, ab_fnl as
+(
+  select
+      ab_tbl.icustay_id, ab_tbl.intime, ab_tbl.outtime
+    , ab_tbl.antibiotic_name
+    , ab_tbl.antibiotic_time
+    , coalesce(me72.charttime,me72.chartdate) as last72_charttime
+    , coalesce(me24.charttime,me24.chartdate) as next24_charttime
+
+    , me72.positiveculture as last72_positiveculture
+    , me72.spec_type_desc as last72_specimen
+    , me24.positiveculture as next24_positiveculture
+    , me24.spec_type_desc as next24_specimen
+  from ab_tbl
+  -- blood culture in last 72 hours
+  left join me me72
+    on ab_tbl.hadm_id = me72.hadm_id
+    and ab_tbl.antibiotic_time is not null
+    and
+    (
+      -- if charttime is available, use it
+      (
+          ab_tbl.antibiotic_time > me72.charttime
+      and ab_tbl.antibiotic_time <= DATEADD(HOUR, 72, me72.charttime)
+      )
+      OR
+      (
+      -- if charttime is not available, use chartdate
+          me72.charttime is null
+      and ab_tbl.antibiotic_time > me72.chartdate
+      and ab_tbl.antibiotic_time < DATEADD(HOUR, 96, me72.chartdate)-- could equally do this with a date_trunc, but that's less portable
+      )
+    )
+  -- blood culture in subsequent 24 hours
+  left join me me24
+    on ab_tbl.hadm_id = me24.hadm_id
+    and ab_tbl.antibiotic_time is not null
+    and me24.charttime is not null
+    and
+    (
+      -- if charttime is available, use it
+      (
+          ab_tbl.antibiotic_time > DATEADD(HOUR, -24, me24.charttime)
+      and ab_tbl.antibiotic_time <= me24.charttime
+      )
+      OR
+      (
+      -- if charttime is not available, use chartdate
+          me24.charttime is null
+      and ab_tbl.antibiotic_time > me24.chartdate
+      and ab_tbl.antibiotic_time <= DATEADD(HOUR, 24, me24.chartdate)
+      )
+    )
+)
+, ab_laststg as
+(
+select
+  icustay_id
+  , antibiotic_name
+  , antibiotic_time
+  , last72_charttime
+  , next24_charttime
+
+  -- time of suspected infection: either the culture time (if before antibiotic), or the antibiotic time
+  , case
+      when coalesce(last72_charttime,next24_charttime) is null
+        then 0
+      else 1 end as suspected_infection
+
+  , coalesce(last72_charttime,next24_charttime) as suspected_infection_time
+
+  -- the specimen that was cultured
+  , case
+      when last72_charttime is not null
+        then last72_specimen
+      when next24_charttime is not null
+        then next24_specimen
+    else null
+  end as specimen
+
+  -- whether the cultured specimen ended up being positive or not
+  , case
+      when last72_charttime is not null
+        then last72_positiveculture
+      when next24_charttime is not null
+        then next24_positiveculture
+    else null
+  end as positiveculture
+from ab_fnl
+)
+select
+  icustay_id
+  , antibiotic_name
+  , antibiotic_time
+  , last72_charttime
+  , next24_charttime
+  , suspected_infection_time
+  -- -- the below two fields are used to extract data - modifying them facilitates sensitivity analyses
+  , DATEADD(HOUR, -48, suspected_infection_time) as si_starttime --CHANGE FROM ORIGINAL: uncommented this and next line such that start and end time are calculated!
+  , DATEADD(HOUR, 24, suspected_infection_time) as si_endtime
+  , specimen, positiveculture
+into dbo.suspinfect_poe_iv
+from ab_laststg
+order by icustay_id, antibiotic_time;
+GO
